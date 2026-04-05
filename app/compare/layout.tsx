8b6067841082623dd4fbabcabb08f56dd5d28c89
Asunoke <cheickoumar871@gmail.com>
2026-04-05 09:51:50 +0000

    fix: remove workaround layout files (framework handles root layout natively now)

--- app/compare/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PageShell } from "../shared-layout";
-
-export default function CompareLayout({ children }: { children: React.ReactNode }) {
-  return <PageShell>{children}</PageShell>;
-}
--- app/docs/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PageShell } from "../shared-layout";
-
-export default function DocsLayout({ children }: { children: React.ReactNode }) {
-  return <PageShell>{children}</PageShell>;
-}
